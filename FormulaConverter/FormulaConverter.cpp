f2c112d3a85984638cc4b1d3f0a66bd3a9a27a3d
takano536 <ohnishi.takanori@gmail.com>
2025-02-26 08:56:23 +0900

    rpnをinfixに変換する機能を追加

--- FormulaConverter/FormulaConverter.cpp
+++ FormulaConverter/FormulaConverter.cpp
@@ -1,9 +1,12 @@
 #include "FormulaConverter.hpp"
+#include "../Tokenizer/Tokenizer.hpp"
 
 #include <algorithm>
 #include <stack>
+#include <stdexcept>
+#include <utility>
 
-const std::map<std::string, int> FormulaConverter::OP_PRECEDENCES_FOR_INFIX = {
+const std::map<std::string, int> FormulaConverter::OP_PRIO_FOR_INFIX = {
     {"+", 1},
     {"-", 1},
     {"*", 2},
@@ -24,12 +27,12 @@ std::vector<std::string> FormulaConverter::infix2rpn(const std::vector<std::stri
             continue;
         }
 
-        const bool is_operator = OP_PRECEDENCES_FOR_INFIX.find(token) != OP_PRECEDENCES_FOR_INFIX.end();
+        const bool is_operator = OP_PRIO_FOR_INFIX.find(token) != OP_PRIO_FOR_INFIX.end();
         if (is_operator) {
             while (!op_stack.empty()) {
                 const auto top = op_stack.top();
-                if (OP_PRECEDENCES_FOR_INFIX.find(top) == OP_PRECEDENCES_FOR_INFIX.end()) break;
-                if (OP_PRECEDENCES_FOR_INFIX.at(token) > OP_PRECEDENCES_FOR_INFIX.at(top)) break;
+                if (OP_PRIO_FOR_INFIX.find(top) == OP_PRIO_FOR_INFIX.end()) break;
+                if (OP_PRIO_FOR_INFIX.at(token) > OP_PRIO_FOR_INFIX.at(top)) break;
                 rpn.push_back(top);
                 op_stack.pop();
             }
@@ -60,4 +63,44 @@ std::vector<std::string> FormulaConverter::infix2rpn(const std::vector<std::stri
     }
 
     return rpn;
+}
+
+std::vector<std::string> FormulaConverter::rpn2infix(const std::vector<std::string>& tokens) {
+    std::vector<std::string> infix;
+    std::stack<std::pair<std::string, int>> stack;
+    auto is_digit = [](const char& c) { return std::isdigit(c) || c == '.'; };
+
+    for (const auto& token : tokens) {
+
+        const bool is_number = std::all_of(token.begin(), token.end(), is_digit);
+        if (is_number) {
+            stack.push({token, INF});
+            continue;
+        }
+
+        const bool is_operator = OP_PRIO_FOR_INFIX.find(token) != OP_PRIO_FOR_INFIX.end();
+        if (is_operator) {
+
+            if (stack.size() < 2) throw std::runtime_error("Invalid formula");
+
+            const auto [rhs, rhs_prio] = stack.top();
+            stack.pop();
+            const auto [lhs, lhs_prio] = stack.top();
+            stack.pop();
+            const int prio = OP_PRIO_FOR_INFIX.at(token);
+
+            std::string lhs_expr = (lhs_prio < prio) ? "(" + lhs + ")" : lhs;
+            std::string rhs_expr = (rhs_prio <= prio) ? "(" + rhs + ")" : rhs;
+
+            stack.push({lhs_expr + " " + token + " " + rhs_expr, prio});
+            continue;
+        }
+
+        throw std::runtime_error("Invalid formula");
+    }
+
+    if (stack.size() != 1) throw std::runtime_error("Invalid formula");
+
+    const auto [expr, _] = stack.top();
+    return Tokenizer::tokenize(expr);
 }
--- FormulaConverter/FormulaConverter.hpp
+++ FormulaConverter/FormulaConverter.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <limits>
 #include <map>
 #include <string>
 #include <vector>
@@ -11,8 +12,10 @@ public:
     FormulaConverter& operator=(const FormulaConverter&) = delete;
 
 private:
-    static const std::map<std::string, int> OP_PRECEDENCES_FOR_INFIX;
+    static const std::map<std::string, int> OP_PRIO_FOR_INFIX;
+    static constexpr int INF = std::numeric_limits<int>::max();
 
 public:
     static std::vector<std::string> infix2rpn(const std::vector<std::string>& tokens);
+    static std::vector<std::string> rpn2infix(const std::vector<std::string>& tokens);
 };
