3bf7a86f9e3c7214899ec6563ef5a39995ef0e69
takano536 <ohnishi.takanori@gmail.com>
2025-02-26 11:13:13 +0900

    クイズ形式にした

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <array>
 #include <iostream>
+#include <random>
 #include <string>
 #include <utility>
 #include <vector>
@@ -36,33 +37,44 @@ int main() {
     static constexpr int TARGET = 10;
     static constexpr int SIZE = 4;
 
-    std::vector<std::string> nums;
-    myfuncs::generate_nums({0, 9}, SIZE, "", nums);
-
     std::vector<std::string> ops;
     myfuncs::generate_nums({0, 3}, SIZE - 1, "", ops);
 
-    std::array<std::vector<std::string>, static_cast<std::size_t>(myfuncs::powi(10, SIZE))> results;
+    std::mt19937 gen(std::random_device{}());
+    std::uniform_int_distribution<int> dist(0, myfuncs::powi(10, SIZE) - 1);
 
-    for (const auto& num : nums) {
+    std::vector<std::string> ans;
+
+    std::string num;
+    while (true) {
+
+        num = std::to_string(dist(gen));
+        while (num.length() < SIZE) num = "0" + num;
+
         for (const auto& op : ops) {
 
-            std::vector<std::string> rpn_tokens;
-            for (const auto& c : op) rpn_tokens.push_back(OPS[c - '0']);
-            for (const auto& c : num) rpn_tokens.push_back(std::string(1, c));
+            std::vector<std::string> tokens;
+            for (const auto& c : op) tokens.push_back(OPS[c - '0']);
+            for (const auto& c : num) tokens.push_back(std::string(1, c));
+            std::sort(tokens.begin(), tokens.end());
 
             do {
+                std::string curr_num;
+                for (const auto& t : tokens) {
+                    if (std::isdigit(t[0])) curr_num += t;
+                }
+                if (curr_num != num) continue;
 
                 std::vector<std::string> infix_tokens;
                 try {
-                    infix_tokens = FormulaConverter::rpn2infix(rpn_tokens);
+                    infix_tokens = FormulaConverter::rpn2infix(tokens);
                 } catch (const std::exception& e) {
                     continue;
                 }
 
                 Fraction result = 0;
                 try {
-                    result = RpnCalculator::calculate(rpn_tokens);
+                    result = RpnCalculator::calculate(tokens);
                 } catch (const std::exception& e) {
                     continue;
                 }
@@ -70,23 +82,53 @@ int main() {
                 if (result != TARGET) continue;
 
                 const auto infix_formula = Tokenizer::join(infix_tokens);
+                ans.push_back(infix_formula);
 
-                std::string curr_num;
-                std::copy_if(infix_formula.begin(), infix_formula.end(), std::back_inserter(curr_num), [](const char& c) { return std::isdigit(c); });
+            } while (std::next_permutation(tokens.begin(), tokens.end()));
+        }
+        if (!ans.empty()) break;
+    }
 
-                results[std::stoi(curr_num)].push_back(infix_formula);
+    while (true) {
+        std::cout << num << " = " << TARGET << "? [a: print ans] > ";
+        std::string input;
+        std::getline(std::cin, input);
 
-            } while (std::next_permutation(rpn_tokens.begin(), rpn_tokens.end()));
+        if (input[0] == 'a') break;
+
+        std::vector<std::string> rpn_tokens;
+        try {
+            rpn_tokens = FormulaConverter::infix2rpn(Tokenizer::tokenize(input));
+        } catch (const std::exception& e) {
+            std::cout << "Invalid input!: FormulaConverter::infix2rpn" << std::endl;
+            continue;
+        }
+
+        Fraction result = 0;
+        try {
+            result = RpnCalculator::calculate(rpn_tokens);
+        } catch (const std::exception& e) {
+            std::cout << "Invalid input!: RpnCalculator::calculate" << std::endl;
+            continue;
+        }
+
+        std::string input_nums;
+        for (const auto& c : input) {
+            if (std::isdigit(c)) input_nums += c;
+        }
+        if (input_nums != num) {
+            std::cout << "Invalid input!" << std::endl;
+            continue;
+        }
+
+        if (result == TARGET) {
+            std::cout << "Correct!" << std::endl;
+            break;
+        } else {
+            std::cout << "Incorrect!" << std::endl;
         }
     }
 
-    for (int i = 0; i < myfuncs::powi(10, SIZE); i++) {
-        if (results[i].empty()) continue;
-
-        std::cout << i << ": " << results[i].size() << '\n';
-        for (const auto& res : results[i]) std::cout << res << '\n';
-        std::cout << std::endl;
-    }
-
+    for (const auto& a : ans) std::cout << a << std::endl;
     return 0;
 }
